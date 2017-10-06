5160a5f4324aa7c239c5804754ac77189e28033b
Li Gewei <ipgw@qq.com>
2017-10-06 11:09:45 +0800

    Format codes

--- single_send_recv/client.c
+++ single_send_recv/client.c
@@ -7,37 +7,38 @@
 
 void error_handling(char *message);
 
-int main(int argc, char* argv[])
+int main(int argc, char *argv[])
 {
 	int sock;
 	struct sockaddr_in serv_addr;
 	char message[32];
 	int str_len;
 	
-	if(argc!=3){
+	if (argc != 3)
+	{
 		printf("Usage : %s <IP> <port>\n", argv[0]);
 		exit(1);
 	}
-	
-	sock=socket(PF_INET, SOCK_STREAM, 0);
-	if(sock < 0)
+
+	sock = socket(PF_INET, SOCK_STREAM, 0);
+	if (sock < 0)
 		error_handling("socket() error");
-	
+
 	memset(&serv_addr, 0, sizeof(serv_addr));
-	serv_addr.sin_family=AF_INET;
-	serv_addr.sin_addr.s_addr=inet_addr(argv[1]);
-	serv_addr.sin_port=htons(atoi(argv[2]));
-		
-	if(connect(sock, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) < 0) 
+	serv_addr.sin_family = AF_INET;
+	serv_addr.sin_addr.s_addr = inet_addr(argv[1]);
+	serv_addr.sin_port = htons(atoi(argv[2]));
+
+	if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)
 		error_handling("connect() error!");
-	
-	str_len=read(sock, message, sizeof(message));
-	if(str_len < 0)
-        error_handling("read() error!");
-    else
-        printf("---recved data len: %d\n", str_len);
-	
-	printf("Message from server: %s \n", message);  
+
+	str_len = read(sock, message, sizeof(message));
+	if (str_len < 0)
+		error_handling("read() error!");
+	else
+		printf("---recved data len: %d\n", str_len);
+
+	printf("Message from server: %s \n", message);
 	close(sock);
 
 	return 0;
--- single_send_recv/server.c
+++ single_send_recv/server.c
@@ -16,37 +16,38 @@ int main(int argc, char *argv[])
 	struct sockaddr_in clnt_addr;
 	socklen_t clnt_addr_size;
 
-	char message[]="Better Man!";
-	
-	if(argc!=2){
+	char message[] = "Better Man!";
+
+	if (argc != 2)
+	{
 		printf("Usage : %s <port>\n", argv[0]);
 		exit(1);
 	}
-	
-	serv_sock=socket(PF_INET, SOCK_STREAM, 0);
-	if(serv_sock < 0)
+
+	serv_sock = socket(PF_INET, SOCK_STREAM, 0);
+	if (serv_sock < 0)
 		error_handling("socket() error");
-	
+
 	memset(&serv_addr, 0, sizeof(serv_addr));
-	serv_addr.sin_family=AF_INET;
-	serv_addr.sin_addr.s_addr=htonl(INADDR_ANY);
-	serv_addr.sin_port=htons(atoi(argv[1]));
-	
-	if(bind(serv_sock, (struct sockaddr*) &serv_addr, sizeof(serv_addr)) < 0)
-		error_handling("bind() error"); 
-	
-	if(listen(serv_sock, 5) < 0)
+	serv_addr.sin_family = AF_INET;
+	serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+	serv_addr.sin_port = htons(atoi(argv[1]));
+
+	if (bind(serv_sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)
+		error_handling("bind() error");
+
+	if (listen(serv_sock, 5) < 0)
 		error_handling("listen() error");
-	
-	clnt_addr_size=sizeof(clnt_addr);  
-	clnt_sock=accept(serv_sock, (struct sockaddr*)&clnt_addr,&clnt_addr_size);
-	if(clnt_sock < 0)
-		error_handling("accept() error");  
-	
-    printf("---ready to write: %s\n", message);
+
+	clnt_addr_size = sizeof(clnt_addr);
+	clnt_sock = accept(serv_sock, (struct sockaddr *)&clnt_addr, &clnt_addr_size);
+	if (clnt_sock < 0)
+		error_handling("accept() error");
+
+	printf("---ready to write: %s\n", message);
 	size_t w_len = write(clnt_sock, message, sizeof(message));
-    printf("---already written len: %d\n", w_len);
-	close(clnt_sock);	
+	printf("---already written len: %d\n", w_len);
+	close(clnt_sock);
 	close(serv_sock);
 
 	return 0;
